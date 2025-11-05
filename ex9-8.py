703500389107610d96daa028119fc1d39279c743
DarkLordGeo <148902362+DarkLordGeo@users.noreply.github.com>
2025-11-05 18:54:26 +0400

    11/5/2025

---
+++ ex9-8.py
@@ -0,0 +1,85 @@
+"""
+9-8. Privileges: Write a separate Privileges class. The class should have one
+attribute, privileges, that stores a list of strings as described in Exercise 9-7.
+Move the show_privileges() method to this class. Make a Privileges instance
+as an attribute in the Admin class. Create a new instance of Admin and use your
+method to show its privileges.
+"""
+
+
+class User:
+    """Represent a simple user profile."""
+
+    def __init__(
+        self, first_name: str, last_name: str, username: str, email: str, location: str
+    ):
+        """Initialize the user."""
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.username = username
+        self.email = email
+        self.location = location.title()
+        self.login_attempts = 0
+
+    def describe_user(self):
+        """Display a summary of the user's information."""
+        print(f"\n  {self.first_name} {self.last_name}")
+        print(f"  Username: {self.username}")
+        print(f"  Email: {self.email}")
+        print(f"  Location: {self.location}")
+
+    def greet_user(self):
+        """Display a personalized greeting to the user."""
+        print(f"\nWelcome back, {self.username}!")
+
+    def increment_login_attempts(self):
+        """Increment the value of login_attempts."""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Reset login_attempts to 0."""
+        self.login_attempts = 0
+
+
+class Privileges:
+    def __init__(self):
+        self.privs = []
+
+    def show_privilegies(self):
+        print("\nPrivilegies:")
+        for p in self.privs:
+            print(f"[*] {p}")
+
+
+class Admin(User):
+    def __init__(self, first_name, last_name, username, email, location):
+        super().__init__(first_name, last_name, username, email, location)
+        self.privileiges = Privileges()
+
+
+admin = Admin(
+    "lasha",
+    "erge",
+    "lerge",
+    "lerge@gmail.com",
+    "Tbilisi",
+)
+admin.describe_user()
+admin.privileiges.privs = ["can ban user", "can modify data"]
+admin.privileiges.show_privilegies()
+
+# class Moderator(User):
+#     def __init__(self, first_name, last_name, username, email, location):
+#         super().__init__(self, first_name, last_name, username, email, location)
+#         self.privilegies = []
+
+
+# moderator = Moderator(
+#     "lasha",
+#     "lasha_erge",
+#     "lasha@gmail.com",
+#     "Tbilsi",
+#     "this",
+# )
+
+# moderator.privilegies = ["Moderator", "User"]
